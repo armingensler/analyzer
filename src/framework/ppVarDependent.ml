3cae66e9cd9528d4dce663c6cdd7582228d7f766
Armin Gensler <armingens@freenet.de>
2015-10-07 16:29:22 +0200

    Removed None leafes from tree. Bottom of underlying Deadcode domain  is now used instead.

--- src/framework/ppVarDependent.ml
+++ src/framework/ppVarDependent.ml
@@ -22,7 +22,7 @@ let varstates_to_string vs =
 type 'd varTree = 
   | VarTree_TF of 'd varTree * 'd varTree
   | VarTree_Both of 'd varTree
-  | VarTree_Leaf of 'd option
+  | VarTree_Leaf of 'd
 
 exception PathResultSplit of int
 
@@ -46,15 +46,7 @@ struct
     
   (* Creates a tree representing a single defined value .*)
   let single d = 
-    make_both_tree H.height (Some d)
-    
-  (* Creates a tree which has a leaf value at a given path and is empty otherwise. *)
-  let rec make_path_tree path leaf = 
-    match path with
-    | (VarState_True :: xs) -> VarTree_TF (make_path_tree xs leaf, make_both_tree (List.length xs) None)
-    | (VarState_False :: xs) -> VarTree_TF (make_both_tree (List.length xs) None, make_path_tree xs leaf)
-    | (VarState_Both :: xs) -> VarTree_Both (make_path_tree xs leaf)
-    | [] -> VarTree_Leaf leaf
+    make_both_tree H.height d
     
   (* Returns the leaf at a given path. If the path is ambiguous a PathResultSplit exception is raised. *)
   let get_or_raise tree path =
@@ -102,10 +94,6 @@ struct
       end in
     impl tree []
   
-  (* Transforms all leaves which are not NONE by applying a function. *)
-  let mapSome f tree = 
-    map (fun leaf path -> if Option.is_some leaf then f (Option.get leaf) path else None) tree
-  
   (* Creates a list of all leaves (including NONE) transformed by some function. *)
   let to_list f tree =
     let rec impl tree path =
@@ -113,17 +101,6 @@ struct
       | VarTree_TF (t, f) -> impl t (path @ [VarState_True]) @ impl f (path @ [VarState_False])
       | VarTree_Both tf -> impl tf (path @ [VarState_Both])
       | VarTree_Leaf leaf -> [f leaf path]
-      end in
-    impl tree []
-  
-  (* Creates a list of all non NONE leaves transformed by some function. *)
-  let to_some_list f tree =
-    let rec impl tree path =
-      begin match tree with
-      | VarTree_TF (t, f) -> impl t (path @ [VarState_True]) @ impl f (path @ [VarState_False])
-      | VarTree_Both tf -> impl tf (path @ [VarState_Both])
-      | VarTree_Leaf (Some leaf) -> [f leaf path]
-      | VarTree_Leaf None -> []
       end in
     impl tree []
     
@@ -147,10 +124,7 @@ struct
     | (VarTree_TF (xt, xf), VarTree_Both ytf) -> VarTree_TF (zip xt ytf, zip xf ytf)
     | (VarTree_Both xtf, VarTree_TF (yt, yf)) -> VarTree_TF (zip xtf yt, zip xtf yf)
     | (VarTree_Both xtf, VarTree_Both ytf) -> VarTree_Both (zip xtf ytf)
-    | (VarTree_Leaf (Some xleaf), VarTree_Leaf (Some yleaf)) -> VarTree_Leaf (Some (xleaf, yleaf))
-    | (VarTree_Leaf (Some xleaf), VarTree_Leaf None) -> VarTree_Leaf None
-    | (VarTree_Leaf None, VarTree_Leaf (Some yleaf)) -> VarTree_Leaf None
-    | (VarTree_Leaf None, VarTree_Leaf None) -> VarTree_Leaf None
+    | (VarTree_Leaf xleaf, VarTree_Leaf yleaf) -> VarTree_Leaf (xleaf, yleaf)
     | (VarTree_Leaf xleaf, _) -> failwith "Called VarTree.zip_strict with inconsistent tree height."
     | (_, VarTree_Leaf yleaf) -> failwith "Called VarTree.zip_strict with inconsistent tree height."
   
@@ -168,8 +142,7 @@ struct
     match d with
     | VarTree_TF (t, f) -> "[ T -> " ^ short i t ^ ", F -> " ^ short i f ^ "]"
     | VarTree_Both tf -> "[ * -> " ^ short i tf ^ "]"
-    | VarTree_Leaf (Some leaf) -> "(" ^ L.short i leaf ^ ")"
-    | VarTree_Leaf None -> "NONE"
+    | VarTree_Leaf leaf -> "(" ^ L.short i leaf ^ ")"
     
   let name () = "VarTreePrintable" ^ (L.name ())
   
@@ -177,13 +150,9 @@ struct
   
   let toXML x = 
     let f leaf path = 
-      match leaf with
-      | Some leaf ->  
-        let flatten_single = function Xml.Element (_,_,[x]) -> Xml.children x | x -> [x] in
-        let children = flatten_single @@ L.toXML leaf in
-        Xml.Element ("Node", [("text", "[" ^ varstates_to_string path ^ "]")], children)
-      | None -> 
-        Xml.Element ("Leaf", [("text", "[" ^ varstates_to_string path ^ "] = NONE")], [])
+      let flatten_single = function Xml.Element (_,_,[x]) -> Xml.children x | x -> [x] in
+      let children = flatten_single @@ L.toXML leaf in
+      Xml.Element ("Node", [("text", "[" ^ varstates_to_string path ^ "]")], children)
     in
     let entries = to_list f x in
     Xml.Element ("Node", [("text", "abcdef")], [Xml.Element ("Node", [("text", "xyzxyz")], entries)])
@@ -196,16 +165,14 @@ struct
     match (x, y) with
     | (VarTree_TF (xtrue, xfalse), VarTree_TF (ytrue, yfalse)) -> equal xtrue ytrue && equal xfalse yfalse 
     | (VarTree_Both xboth, VarTree_Both yboth) -> equal xboth yboth
-    | (VarTree_Leaf (Some xleaf), VarTree_Leaf (Some yleaf)) -> L.equal xleaf yleaf
-    | (VarTree_Leaf None, VarTree_Leaf None) -> true
+    | (VarTree_Leaf xleaf, VarTree_Leaf yleaf) -> L.equal xleaf yleaf
     | _ -> false
   
   let rec hash d =
     match d with
     | VarTree_TF (t, f) -> hash t * 7 + hash f * 13 + 17
     | VarTree_Both both -> hash both * 7 + 23
-    | VarTree_Leaf (Some leaf) -> L.hash leaf * 3
-    | VarTree_Leaf None -> 31
+    | VarTree_Leaf leaf -> L.hash leaf * 3
 
   (* Simplifies the root node of the tree by merging TF nodes with identical children into a
      single Both node. *)
@@ -225,36 +192,9 @@ struct
     | VarTree_Both tf -> VarTree_Both (simplify tf)
     | VarTree_Leaf leaf -> VarTree_Leaf leaf
   
-  (* Sets all leaves of the tree for which the variable[pos] <> value to None. *) 
-  let restrict pos value (tree : t) =
-    let rec impl tree i =
-      if i < pos then begin
-        match tree with
-        | VarTree_TF (t, f) -> simplify_nonrec @@ VarTree_TF (impl t (i + 1), impl f (i + 1))
-        | VarTree_Both tf -> VarTree_Both (impl tf (i + 1))
-        | VarTree_Leaf leaf -> failwith "Called VarTreePrintable.restrict with inconsistent tree height."
-      end else begin
-        match tree with
-        | VarTree_TF (t, f) -> 
-          if value 
-          then simplify_nonrec @@ VarTree_TF (t, make_both_tree (H.height - pos - 1) None) 
-          else simplify_nonrec @@ VarTree_TF (make_both_tree (H.height - pos - 1) None, f)
-        | VarTree_Both tf -> 
-          if value 
-          then simplify_nonrec @@ VarTree_TF (tf, make_both_tree (H.height - pos - 1) None) 
-          else simplify_nonrec @@ VarTree_TF (make_both_tree (H.height - pos - 1) None, tf)
-        | VarTree_Leaf leaf -> VarTree_Leaf leaf
-      end
-    in
-    impl tree 0
-  
   (* Returns a string showing all mappings in this tree without the actual tree structure. *)
   let to_content_string tree =
-    let f leaf path = 
-      match leaf with
-      | None -> varstates_to_string path ^ " -> NONE"
-      | Some x -> varstates_to_string path ^ " -> (" ^ L.short 80 x ^ ")" 
-    in
+    let f leaf path = varstates_to_string path ^ " -> (" ^ L.short 80 leaf ^ ")" in
     "[" ^ (String.concat ", " @@ to_list f tree) ^ "]"
   
 end
@@ -290,20 +230,17 @@ struct
       
       | VarTree_Leaf leaf -> 
         begin match path with
-        | [] -> Option.is_some leaf && L.leq d (Option.get leaf)
+        | [] -> L.leq d leaf
         | _ -> failwith "Called VarTreeDom.leq with inconsistent tree heights."
         end
       
     in
-    let f leaf path = Option.is_none leaf || leq_than_all_at_path ytree (Option.get leaf) path in
+    let f leaf path = leq_than_all_at_path ytree leaf path in
     for_all f xtree
     
   let rec join x y = 
     match (x, y) with
-    | (VarTree_Leaf (Some xleaf), VarTree_Leaf (Some yleaf)) -> VarTree_Leaf (Some (L.join xleaf yleaf))
-    | (VarTree_Leaf (Some xleaf), VarTree_Leaf None) -> VarTree_Leaf (Some xleaf)
-    | (VarTree_Leaf None, VarTree_Leaf (Some yleaf)) -> VarTree_Leaf (Some yleaf)
-    | (VarTree_Leaf None, VarTree_Leaf None) -> VarTree_Leaf None
+    | (VarTree_Leaf xleaf, VarTree_Leaf yleaf) -> VarTree_Leaf (L.join xleaf yleaf)
     | (VarTree_Leaf xleaf, _) -> failwith "Called VarTreeDom.join with inconsistent tree heights."
     | (_, VarTree_Leaf yleaf) -> failwith "Called VarTreeDom.join with inconsistent tree heights."
     | (VarTree_TF (xt, xf), VarTree_TF (yt, yf)) -> simplify_nonrec @@ VarTree_TF (join xt yt, join xf yf)
@@ -313,10 +250,7 @@ struct
     
   let rec meet x y = 
     match (x, y) with
-    | (VarTree_Leaf (Some xleaf), VarTree_Leaf (Some yleaf)) -> VarTree_Leaf (Some (L.meet xleaf yleaf))
-    | (VarTree_Leaf (Some xleaf), VarTree_Leaf None) -> VarTree_Leaf None
-    | (VarTree_Leaf None, VarTree_Leaf (Some yleaf)) -> VarTree_Leaf None
-    | (VarTree_Leaf None, VarTree_Leaf None) -> VarTree_Leaf None    
+    | (VarTree_Leaf xleaf, VarTree_Leaf yleaf) -> VarTree_Leaf (L.meet xleaf yleaf)
     | (VarTree_Leaf xleaf, _) -> failwith "Called VarTreeDom.meet with inconsistent tree heights."
     | (_, VarTree_Leaf yleaf) -> failwith "Called VarTreeDom.meet with inconsistent tree heights."
     | (VarTree_TF (xt, xf), VarTree_TF (yt, yf)) -> simplify_nonrec @@ VarTree_TF (meet xt yt, meet xf yf)
@@ -324,13 +258,13 @@ struct
     | (VarTree_Both xtf, VarTree_TF (yt, yf)) -> simplify_nonrec @@ VarTree_TF (meet xtf yt, meet xtf yf)
     | (VarTree_Both xtf, VarTree_Both ytf) -> VarTree_Both (meet xtf ytf)
     
-  let bot () = make_both_tree H.height None
+  let bot () = single (L.bot ())
   
-  let is_bot tree = for_all (fun leaf fs -> Option.is_none leaf) tree
-    
-  let top () = make_both_tree H.height (Some (L.top ()))
+  let is_bot tree = for_all (fun leaf fs -> L.is_bot leaf) tree
   
-  let is_top tree = for_all (fun leaf fs -> Option.is_some leaf && L.is_top (Option.get leaf)) tree
+  let top () = single (L.top ())
+  
+  let is_top tree = for_all (fun leaf fs -> L.is_top leaf) tree
   
   let rec joined_fs_path tree path =
     match (tree, path) with
@@ -340,12 +274,42 @@ struct
     | (VarTree_TF (t, f), []) -> failwith "VarTreePrintable.get with too short fs"
     | (VarTree_Both tf, x::xs) -> joined_fs_path tf xs
     | (VarTree_Both tf, []) -> failwith "VarTreePrintable.get with too short fs"
-    | (VarTree_Leaf (Some leaf), []) -> leaf
-    | (VarTree_Leaf None, []) -> L.bot ()
+    | (VarTree_Leaf leaf, []) -> leaf
     | (VarTree_Leaf leaf, _) -> failwith "VarTreePrintable.get with too long fs"
     
-  let make_from_list xs =
-    List.fold_left join (bot ()) @@ List.map (fun (path, d) -> make_path_tree path (Some d)) xs
+  (* Creates a tree which has a leaf value at a given path and is empty otherwise. *)
+  let rec make_path_tree path leaf = 
+    match path with
+    | (VarState_True :: xs) -> VarTree_TF (make_path_tree xs leaf, make_both_tree (List.length xs) (L.bot ()))
+    | (VarState_False :: xs) -> VarTree_TF (make_both_tree (List.length xs) (L.bot ()), make_path_tree xs leaf)
+    | (VarState_Both :: xs) -> VarTree_Both (make_path_tree xs leaf)
+    | [] -> VarTree_Leaf leaf
+  
+  (* Sets all leaves of the tree for which the variable[pos] <> value to bottom. *) 
+  let restrict pos value (tree : t) =
+    let rec impl tree i =
+      if i < pos then begin
+        match tree with
+        | VarTree_TF (t, f) -> simplify_nonrec @@ VarTree_TF (impl t (i + 1), impl f (i + 1))
+        | VarTree_Both tf -> VarTree_Both (impl tf (i + 1))
+        | VarTree_Leaf leaf -> failwith "Called VarTreePrintable.restrict with inconsistent tree height."
+      end else begin
+        match tree with
+        | VarTree_TF (t, f) -> 
+          if value 
+          then simplify_nonrec @@ VarTree_TF (t, make_both_tree (H.height - pos - 1) (L.bot ())) 
+          else simplify_nonrec @@ VarTree_TF (make_both_tree (H.height - pos - 1) (L.bot ()), f)
+        | VarTree_Both tf -> 
+          if value 
+          then simplify_nonrec @@ VarTree_TF (tf, make_both_tree (H.height - pos - 1) (L.bot ())) 
+          else simplify_nonrec @@ VarTree_TF (make_both_tree (H.height - pos - 1) (L.bot ()), tf)
+        | VarTree_Leaf leaf -> VarTree_Leaf leaf
+      end
+    in
+    impl tree 0
+    
+  let make_from_list (xs : (varState list * L.t) list) =
+    List.fold_left join (bot ()) @@ List.map (fun (path, d) -> make_path_tree path d) xs
     
 end
 
@@ -383,7 +347,7 @@ struct
     (assign_acc : (varState list * string option * lval * exp) list ref) =
     
     let rec ask q = S.query ctx' q
-    and global v = Option.default (S.G.bot ()) @@ G.get_or_raise (ctx.global v) path
+    and global v = G.get_or_raise (ctx.global v) path
     
     and spawn v d = spawn_acc := (path, v, d) :: !spawn_acc
     and split d e b = split_acc := (path, d, e, b) :: !split_acc
@@ -498,31 +462,23 @@ struct
   let startstate v = D.single @@ S.startstate v
   let exitstate v = D.single @@ S.exitstate v
   let otherstate v = D.single @@ S.otherstate v
-  let morphstate v ds = D.simplify @@ D.mapSome (fun d path -> Some (S.morphstate v d)) ds
+  let morphstate v ds = D.simplify @@ D.map (fun d path -> S.morphstate v d) ds
   
-  let should_join (xs : D.t) (ys : D.t) = 
-    print_endline "should_join";
-    let f x y path = 
-      match (x, y) with
-      | (Some x, Some y) -> S.should_join x y
-      | (None, None) -> true
-      | _ -> false
-    in
-    D.for_all_2 f xs ys
+  let should_join xs ys = 
+    D.for_all_2 (fun x y path -> S.should_join x y) xs ys
   
-  let val_of ds = D.simplify @@ C.map (fun leaf path -> Option.map (fun d -> S.val_of d) leaf) ds
-  let context ds = C.simplify @@ D.map (fun leaf path -> Option.map (fun d -> S.context d) leaf) ds
+  let val_of ds = D.simplify @@ C.map (fun leaf path -> S.val_of leaf) ds
+  let context ds = C.simplify @@ D.map (fun leaf path -> S.context leaf) ds
   
   let call_descr func ds = 
-    print_endline "call_descr";
-    let leaves = D.to_some_list (fun leaf path -> leaf) ds in
+    let leaves = D.to_list (fun leaf path -> leaf) ds in
     let head = List.hd leaves in (* ??? *)
     S.call_descr func head (* ??? *)
     
   let do_spawn ctx (xs : (varState list * varinfo * S.D.t) list) =
     let spawn_one (v : varinfo) (ds : (varState list * S.D.t) list) =
       let join_vals (path, d) = path, List.fold_left S.D.join (S.D.bot ()) d in
-      let make_tree (path, d) = D.make_path_tree path (Some d) in
+      let make_tree (path, d) = D.make_path_tree path d in
       let joined = List.fold_left (D.join) (D.bot ()) @@ List.map make_tree @@ List.map join_vals @@ group_assoc ds in
       ctx.spawn v joined
     in
@@ -544,7 +500,7 @@ struct
   let do_sideg ctx (xs : (varState list * varinfo * S.G.t) list) =
     let spawn_one v (ds : (varState list * S.G.t) list) = 
       let join_vals (path, d) = path, List.fold_left S.G.join (S.G.bot ()) d in
-      let make_tree (path, d) = G.make_path_tree path (Some d) in
+      let make_tree (path, d) = G.make_path_tree path d in
       let joined = List.fold_left (G.join) (G.bot ()) @@ List.map make_tree @@ List.map join_vals @@ group_assoc ds in
       ctx.sideg v joined
     in
@@ -568,8 +524,7 @@ struct
           try VarTree_Both (impl tf (path @ [VarState_Both]) (i + 1)) 
           with PathResultSplit split_idx when split_idx = i -> impl (VarTree_TF (tf, tf)) path i
         end
-      | VarTree_Leaf (Some leaf) -> VarTree_Leaf (Some (f leaf path))
-      | VarTree_Leaf None -> VarTree_Leaf None
+      | VarTree_Leaf leaf -> VarTree_Leaf (f leaf path)
       end 
     in
     
@@ -586,24 +541,20 @@ struct
     let handle_child d path = S.sync (child_ctx d path ctx spawnAcc splitAcc sidegAcc assignAcc) in
     let result = execute_for_each_child handle_child ctx.local in
     
-    let dtree = D.simplify @@ D.mapSome (fun (x, y) path -> Some x) result in
-    let gtree = D.mapSome (fun (x, y) path -> Some y) result in
+    let dtree = D.simplify @@ D.map (fun (x, y) path -> x) result in
+    let gtree = D.map (fun (x, y) path -> y) result in
     
     (* get all varinfos in gtree *)
-    let varinfo_collector leaf path = 
-      match leaf with
-      | None -> []
-      | Some d -> List.map fst d
-    in
+    let varinfo_collector leaf path = List.map fst leaf in
     let varinfos = List.unique ~eq:Basetype.Variables.equal @@ List.flatten @@ D.to_list varinfo_collector gtree in
     
     (* get tree for each varinfo *)
     let get_varinfo_tree (var : varinfo) =
       let join_all gs = List.fold_left S.G.join (S.G.bot ()) gs in
-      let joined leaf path = Some (join_all @@ List.map snd @@ List.filter (fun (v, g) -> Basetype.Variables.equal v var) leaf) in
-      G.simplify @@ D.mapSome joined gtree
+      let joined leaf path = join_all @@ List.map snd @@ List.filter (fun (v, g) -> Basetype.Variables.equal v var) leaf in
+      G.simplify @@ D.map joined gtree
     in
-    let g_result = List.map (fun v -> v, get_varinfo_tree v) varinfos in
+    let g_result = List.map (fun v -> (v, get_varinfo_tree v)) varinfos in
     
     do_spawn ctx (!spawnAcc);
     do_split ctx (!splitAcc);
@@ -622,11 +573,7 @@ struct
     let handle_child d path = S.query (child_ctx d path ctx spawnAcc splitAcc sidegAcc assignAcc) q in
     let resultTree = execute_for_each_child handle_child ctx.local in
     
-    let join_leaf acc leaf path =
-      match leaf with
-      | Some d -> Queries.Result.join acc d
-      | None -> acc 
-    in
+    let join_leaf acc leaf path = Queries.Result.join acc leaf in
     let result = T.fold join_leaf (Queries.Result.bot ()) resultTree in
     
     do_spawn ctx (!spawnAcc);
@@ -677,9 +624,7 @@ struct
       | Some (var, negated) ->
         begin match List.index_of var pp_vars with
         | None -> result
-        | Some idx ->
-          print_endline ("simple variable branch: [" ^ var ^ "@" ^ string_of_int idx ^ " == " ^ string_of_bool (tv = negated) ^ "]");
-          D.restrict idx (tv = negated) result
+        | Some idx -> D.restrict idx (tv = negated) result
         end
     in
     
@@ -773,9 +718,9 @@ struct
     let handle_child d path = S.enter (child_ctx d path ctx spawnAcc splitAcc sidegAcc assignAcc) r f args in
     let result = execute_for_each_child handle_child ctx.local in
     
-    let num = List.max @@ T.to_some_list (fun ds path -> List.length ds) result in
+    let num = List.max @@ T.to_list (fun ds path -> List.length ds) result in
     
-    let treelist = T.to_some_list (fun ds path -> (path, ds)) result in
+    let treelist = T.to_list (fun ds path -> (path, ds)) result in
     
     let get_nth n = 
       let nth = List.map (fun (path, ds) -> (path, List.nth ds n)) @@ List.filter (fun (path, ds) -> List.length ds > n) treelist in
